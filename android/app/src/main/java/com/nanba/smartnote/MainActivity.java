895e96a0dc64ab35d4a01e688f068d3c3edb0ea8
HG12265 <gowtham114411@gmail.com>
2026-03-31 17:11:52 +0530

    Fixed file name mismatch in Java

--- android/app/src/main/java/com/nanba/smartnote/MainActivity.java
+++ android/app/src/main/java/com/nanba/smartnote/MainActivity.java
@@ -1,5 +1,36 @@
 package com.nanba.smartnote;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String text = intent.getStringExtra("notificationText");
+            // React app-ku text-ah anupputhu
+            getBridge().getWebView().evaluateJavascript(
+                "window.dispatchEvent(new CustomEvent('notificationReceived', { detail: { text: '" + text + "' } }));",
+                null
+            );
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Receiver-ah register pannu
+        registerReceiver(receiver, new IntentFilter("com.nanba.smartnote.NOTIFICATION_RECEIVED"), Context.RECEIVER_EXPORTED);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
+    }
+}
--- android/app/src/main/java/com/nanba/smartnote/NotificationService.java
+++ android/app/src/main/java/com/nanba/smartnote/NotificationService.java
@@ -1,36 +1,25 @@
 package com.nanba.smartnote;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
-import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    
-    private BroadcastReceiver receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String text = intent.getStringExtra("notificationText");
-            // React app-ku data-va anupputhu
-            getBridge().getWebView().evaluateJavascript(
-                "window.dispatchEvent(new CustomEvent('notificationReceived', { detail: { text: '" + text + "' } }));",
-                null
-            );
+public class NotificationService extends NotificationListenerService {
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        String packageName = sbn.getPackageName();
+        
+        // GPay notification check
+        if (packageName.equals("com.google.android.apps.nbu.paisa")) {
+            Bundle extras = sbn.getNotification().extras;
+            String text = extras.getString("android.text");
+
+            if (text != null) {
+                Intent intent = new Intent("com.nanba.smartnote.NOTIFICATION_RECEIVED");
+                intent.putExtra("notificationText", text);
+                sendBroadcast(intent);
+            }
         }
-    };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Receiver-ah register pannu
-        registerReceiver(receiver, new IntentFilter("com.nanba.smartnote.NOTIFICATION_RECEIVED"), Context.RECEIVER_EXPORTED);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(receiver);
     }
 }
